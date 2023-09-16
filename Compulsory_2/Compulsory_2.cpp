acc54fcbeae8727155f60dda3071f2c7f1b84cb0
Arafat <269424@stud.inn.no>
2023-09-16 19:17:02 +0200

    Finished making case for factorial and a case for simple math. And also a way to exit for both cases and from the main menu

--- Compulsory_2/Compulsory_2.cpp
+++ Compulsory_2/Compulsory_2.cpp
@@ -7,18 +7,38 @@
 
 void main()
 {
-	bool runProgram = false;
+	//for the main menu
+	int mainMenuChoice;
+
+	//for factorial case
+	std::string positiveNumber;
+
+	//for simple math case
+	std::string mathChoice;
+	int num1;
+	int num2;
+	int sum;
+	
 	//main menu
 	do{
-		int mainMenuChoice;
+		
 
-		std::cout << " make a choice \n";
+		std::cout << " make a choice (type 9 to exit) \n";
 		std::cin >> mainMenuChoice;
-		//int validatedChoice = validateInput(mainMenuChoice);
 		switch (mainMenuChoice)
 		{
 		case 1:
 			//factorial
+			do
+			{
+				std::cout << "Enter a positive number (type 'e' to exit)\n";
+				std::cin >> positiveNumber;
+				if(checkInput(positiveNumber) && std::stoi(positiveNumber)>0){
+					int result = factorial(std::stoi(positiveNumber));
+					std::cout << "Factorial of " << positiveNumber << " = " << result << "\n";
+				}
+
+			} while (positiveNumber != "e");
 			break;
 
 		case 2:
@@ -26,23 +46,86 @@ void main()
 			break;
 
 		case 3:
-			//Addition, subtraction, division and multiplication
-			break;
+			
+			do
+			{
+				std::cout << "\nChoose(1-4, or type 'e' to exit): \n";
+				std::cout << "1.Addition\n";
+				std::cout << "2.Subtraction\n";
+				std::cout << "3.Division\n";
+				std::cout << "4.Multiplication\n";
+
+				std::cin >> mathChoice;
+				if (checkInput(mathChoice) && std::stoi(mathChoice) < 5) {
+					switch (std::stoi(mathChoice))
+					{
+						//Addition
+					case 1:
+						std::cout << "Enter first number: ";
+						std::cin >> num1;
+						std::cout << "Enter second number: ";
+						std::cin >> num2;
+
+						sum = addition(num1, num2);
+						std::cout << "\nThe sum is " << sum;
+						break;
+
+						//Division
+					case 2:
+						std::cout << "Enter first number: ";
+						std::cin >> num1;
+						std::cout << "Enter second number: ";
+						std::cin >> num2;
+
+						sum = division(num1, num2);
+						std::cout << "\nThe sum is " << sum;
+						break;
+
+
+
+						//Subtraction
+					case 3:
+						std::cout << "Enter first number: ";
+						std::cin >> num1;
+						std::cout << "Enter second number: ";
+						std::cin >> num2;
+
+						sum = subtraction(num1, num2);
+						std::cout << "\nThe sum is " << sum;
+						break;
+
+
+						//Multiplication
+					case 4:
+						std::cout << "Enter first number: ";
+						std::cin >> num1;
+						std::cout << "Enter second number: ";
+						std::cin >> num2;
+
+						sum = multiplication(num1, num2);
+						std::cout << "\nThe sum is " << sum;
+						break;
+
+
+					default:
+						break;
+					}
+				}
+			} while (mathChoice != "e");
+				break;
 
 		case 9:
-			//end program
-			runProgram = false;
 			break;
-			
+
 		default:
 			//if wrong type
 			std::cout << "wrong answer\n";
-			runProgram = true;
+			std::cin >> mainMenuChoice;
+			//runProgram = true;
+			std::cout << mainMenuChoice;
 			break;
 		}
 		
-	} while (runProgram);
-
-	
+	} while (mainMenuChoice != 9);
 
 }
--- Compulsory_2/Header.h
+++ Compulsory_2/Header.h
@@ -5,8 +5,16 @@
 #include<sstream>
 
 
+//Returns the Factorial of a number.
+int factorial(int x) {
+	if (x > 1) {
+		return x * factorial(x - 1);
+	}
+	else {
+		return 1;
+	}
 
-int factorial() {
+	
 	return 0;
 }
 
@@ -14,7 +22,37 @@ int polynomial() {
 	return 0;
 
 }
-
-int quickMath() {
-	return 0;
+// Adds two numbers together
+int addition(int a,int b) {
+	int sum = a + b;
+	return sum;
 }
+
+// Divides a number with another 
+int division(int dividend, int divisor) {
+	int quotient = dividend / divisor;
+	return quotient;
+}
+
+//Product of two numbers multiplied
+int multiplication(int a, int b) {
+	int sum = a * b;
+	return sum;
+}
+//subtracts an amount from a number
+int subtraction(int a, int b) {
+	int sum = a - b;
+	return sum;
+}
+
+//Checks if the input contains string, returns false if so
+bool checkInput(std::string userInput) {
+	for (int i = 0; i < userInput.length(); i++)
+	{
+		if (!isdigit(userInput[i])) {
+			return false;
+		}
+	}
+	return true;	
+}
+
