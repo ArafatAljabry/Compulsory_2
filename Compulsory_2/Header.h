fb7738260f989368fa77ca2b2a0a2ba70ea9dc18
Arafat <269424@stud.inn.no>
2023-09-16 16:01:56 +0200

    Made the wireframe of the menu.

---
+++ Compulsory_2/Header.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include<sstream>
+
+
+
+int factorial() {
+	return 0;
+}
+
+int polynomial() {
+	return 0;
+
+}
+
+int quickMath() {
+	return 0;
+}
